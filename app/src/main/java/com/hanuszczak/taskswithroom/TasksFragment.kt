cda7038ea9f4e554d288b2c275ac4a2c79e4913f
ahanuszczak <hanyhusajn@gmail.com>
2023-03-15 22:49:51 +0100

    Navigate to EditTaskFragment

--- app/src/main/java/com/hanuszczak/taskswithroom/TasksFragment.kt
+++ app/src/main/java/com/hanuszczak/taskswithroom/TasksFragment.kt
@@ -5,8 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.hanuszczak.taskswithroom.adapter.TaskItemAdapter
 import com.hanuszczak.taskswithroom.databinding.FragmentTasksBinding
 
@@ -31,13 +31,21 @@ class TasksFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
 
         val adapter = TaskItemAdapter { taskId ->
-            Toast.makeText(context, "Clicked task $taskId", Toast.LENGTH_SHORT).show()
+            viewModel.onTaskClicked(taskId)
         }
         binding.tasksList.adapter = adapter
 
         viewModel.tasks.observe(viewLifecycleOwner) {
             it?.let {
                 adapter.submitList(it)
+            }
+        }
+
+        viewModel.navigateToTask.observe(viewLifecycleOwner) {taskId ->
+            taskId?.let {
+                val action = TasksFragmentDirections.actionTasksFragmentToEditTaskFragment(taskId)
+                this.findNavController().navigate(action)
+                viewModel.onTaskNavigated()
             }
         }
 
